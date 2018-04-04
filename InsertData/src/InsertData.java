846388a05f702eda5f4e07955b56d9059a36b55a
shinyou <shinyou@gmail.com>
2018-04-04 07:57:20 -0400

    Update on insertOne

--- InsertData/src/InsertData.java
+++ InsertData/src/InsertData.java
@@ -1,40 +1,85 @@
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.bson.Document;
+//import org.json.JSONObject;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import java.util.Arrays;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.WriteConcern;
+import com.mongodb.WriteResult;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 import static com.mongodb.client.model.Filters.eq;
-
+import static com.mongodb.client.model.Filters.in;
 
 public class InsertData {
-	/*  public static void main(String[] args)
+	/*	  public static void main(String[] args)
 	{
 	System.out.println("hello");
-		String CONNECTION = "mongodb+srv://m001-student:student123#@sandbox-trhqa.mongodb.net/test";
+		/*String CONNECTION = "mongodb+srv://m001-student:student123#@sandbox-trhqa.mongodb.net/test";
 		List<Document> check = AddOneOrder(CONNECTION);//, String[] cities)
 		List<Document> checkAll = AddMultipleOrders(CONNECTION);//, String[] cities)
-		
+		AddOneOrderJsonFile
 		System.out.println(check);
 		System.out.println(checkAll);
-	}
-	*/
+	
+	String CONNECTION = "mongodb+srv://m001-student:student123#@sandbox-trhqa.mongodb.net/test";
+	List<Document> checkFile = AddOneOrderJsonFile(CONNECTION);
+	//List<Document> checkFile2 = AddMultipleOrder(CONNECTION);
+	System.out.println(checkFile);
+	}*/
+	
 	
 	/**
 	 * Inserts documents into the collection using insertOne()
 	 * @param connectionString: To MongoDB instance/MongoDB Cluster.
-	 * @param cities: array of Shipping address cities to filter documents.
+	 * @param addOrder: Json data in String format
 	 * @return List<Document>: ArrayList of matching documents.
 	 */
-	public static List<Document> AddOneOrder(String connectionString)
+	public static List<Document> AddOneOrderWithData(String connectionString, Document addOrder)
 	{
-
+		if(connectionString == null || connectionString.isEmpty() )
+		{
+			throw new IllegalArgumentException();
+		}
+		MongoClientURI clientUri = new MongoClientURI(connectionString);
+		try(MongoClient client = new MongoClient(clientUri))
+		{
+			MongoDatabase database = client.getDatabase("stores");
+			MongoCollection<Document> collection = database.getCollection("orders");		
+	    			
+			collection.insertOne(addOrder);
+			List<Document> queryResult = collection.find().into(new ArrayList<Document>());
+			
+			
+			
+			return queryResult;
+		}
+		catch (Exception e) {
+			//log the exception	
+			System.out.println("An exception occured");
+			System.out.println("Details:");
+			e.printStackTrace();
+			return null;
+		}		
+	}
+	
+	/**
+	 * Read a JSON file to inserts a document into the collection using insertOne()
+	 * @param connectionString: To MongoDB instance/MongoDB Cluster.
+	 * @param AddOrder: A Json File name.
+	 * @return List<Document>: ArrayList of matching documents.
+	 */
+	
+	public static List<Document> AddOneOrderWithExpData(String connectionString, String AddOrder)
+	{
 		if(connectionString == null || connectionString.isEmpty() )
 		{
 			throw new IllegalArgumentException();
@@ -45,178 +90,66 @@ public class InsertData {
 		
 			MongoDatabase database = client.getDatabase("stores");
 			MongoCollection<Document> collection = database.getCollection("orders");		
-        
-	        Document newOrder = new Document("orderPlaced", "2018-03-01")
-	        .append("total", 1633.2)
-	        .append("subtotal",421.42)
-			.append("shipping", 91)
-			.append("tax", 11.41);
-	
-	     	List status = Arrays.asList("shipped","2018-03-10");
-			newOrder.put("status", status);
-	
-			Document shipping = new Document ("number",113)
-			.append("street", "Sunnyside")
-			.append("city","Yonker")
-			.append("state","New York")
-			.append("country","USA")
-			.append("postalCode",57850);
-			newOrder.put("shippingAddress", shipping);
+					
+			Document document = Document.parse(AddOrder);
+			collection.insertOne(document);			
 			
-			
-			Document itemDetail = new Document("sku","MDBTS285")
-			.append("name","color pencil")
-			.append("quantity",32)
-			.append("unit_price",9.3808);
-			List items = Arrays.asList(itemDetail);
-			newOrder.put("lineitems", items);
-			
-			 collection.insertOne(newOrder);
-			
-			 
-	        List<Document> queryResult = collection.find(eq("orderPlaced", "2018-03-01")).into(new ArrayList<Document>());
-				return queryResult;
-	
+			List<Document> queryResult = collection.find().into(new ArrayList<Document>());
+			return queryResult;
 		}
+		
 		catch (Exception e) {
 			//log the exception
 			
 			System.out.println("An exception occured");
 			System.out.println("Details:");
-			System.out.println(e.getStackTrace());
+			e.printStackTrace();
 			return null;
-							
-		}		
+		}	
 	}
 	
 	
-	public static List<Document> AddOneOrderJson(String connectionString)
+	
+	public static List<Document> AddOneOrderJsonFile(String connectionString, String fileName)
 	{
-		
 		if(connectionString == null || connectionString.isEmpty() )
 		{
 			throw new IllegalArgumentException();
 		}
 		MongoClientURI clientUri = new MongoClientURI(connectionString);
-		try(MongoClient client = new MongoClient(clientUri))
+		try (MongoClient client = new MongoClient(clientUri))
 		{
-		
+		   
 			MongoDatabase database = client.getDatabase("stores");
 			MongoCollection<Document> collection = database.getCollection("orders");		
 			
-			
-			String json = "{ 'orderPlaced': '2018-03-01', 'total': 13.00, 'subtotal': 191.00,"
-					+ "'shipping':11.00,'tax':17.00,"
-					+ "'status': ['shipped', '2018-03-04'],"
-					+ "'shippingAddress': {'number': 345, 'street': 'Broadway', 'city': 'Sunnyside',"
-					+"'state':'Yonkers','country':'USA','postalCode':10010},"
-					+"'lineitems':[{'sku':'MDBTS115','name':'color pencil','quantity':31,'unit_price':1.35},"
-					+"{'sku':'MDBTS438','name':'Highlighter','quantity':27,'unit_price':2.4}]}";
-					
-					Document document = Document.parse(json);
-					collection.insertOne(document);			
-
-			         List<Document> queryResult = collection.find(eq("orderPlaced", "2018-03-02")).into(new ArrayList<Document>());
-						return queryResult;
-		
+			//read the json file   
+	        Object obj = new JSONParser().parse(new FileReader(fileName));
+	        System.out.println("DONE");
+	        System.out.println(obj);
+	                
+	        JSONObject jo = (JSONObject) obj;
+	        String jsonText = jo.toJSONString();
+	        Document doc = Document.parse(jsonText);
+		    collection.insertOne(doc);
+		          
+		     List<Document> queryResult = collection.find().into(new ArrayList<Document>());
+			 return queryResult;
+	        
         }
 		catch (Exception e) {
 			//log the exception
 			
 			System.out.println("An exception occured");
 			System.out.println("Details:");
-			System.out.println(e.getStackTrace());
+			e.printStackTrace();
 			return null;
 							
-		}	
+		}
+			
 	}
 	
-	public static List<Document> AddMultipleOrders(String connectionString)
-	{
-
-		if(connectionString == null || connectionString.isEmpty() )
-		{
-			throw new IllegalArgumentException();
-		}
-		MongoClientURI clientUri = new MongoClientURI(connectionString);
-		try(MongoClient client = new MongoClient(clientUri))
-		{
-		
-			MongoDatabase database = client.getDatabase("stores");
-			MongoCollection<Document> collection = database.getCollection("orders");		
-			
-       Document newOrders = new Document();
-        
-        Document newOrder = new Document("orderPlaced", "2018-03-01")
-        .append("total", 1633.2)
-        .append("subtotal",421.42)
-		.append("shipping", 91)
-		.append("tax", 11.41);
-
-     	List status = Arrays.asList("shipped","2018-03-10");
-		newOrder.put("status", status);
-
-		Document shipping = new Document ("number",113)
-		.append("street", "Sunnyside")
-		.append("city","Yonker")
-		.append("state","New York")
-		.append("country","USA")
-		.append("postalCode",57850);
-		newOrder.put("shippingAddress", shipping);
-		
-		
-		Document itemDetail = new Document("sku","MDBTS285")
-		.append("name","color pencil")
-		.append("quantity",32)
-		.append("unit_price",9.3808);
-		List items = Arrays.asList(itemDetail);
-		newOrder.put("lineitems", items);
-		
-		
-		  Document newOrder2 = new Document("orderPlaced", "2018-03-01")
-	        .append("total", 1633.2)
-	        .append("subtotal",421.42)
-			.append("shipping", 91)
-			.append("tax", 11.41);
-
-	     	List status2 = Arrays.asList("shipped","2018-03-10");
-			newOrder.put("status", status2);
-
-			Document shipping2 = new Document ("number",113)
-			.append("street", "Sunnyside")
-			.append("city","Yonker")
-			.append("state","New York")
-			.append("country","USA")
-			.append("postalCode",57850);
-			newOrder.put("shippingAddress", shipping2);
-			
-			
-			Document itemDetail2 = new Document("sku","MDBTS285")
-			.append("name","color pencil")
-			.append("quantity",32)
-			.append("unit_price",9.3808);
-			List items2 = Arrays.asList(itemDetail);
-			newOrder.put("lineitems", items2);
-		
-		 collection.insertMany(Arrays.asList(newOrder,newOrder2));
-		
-		 
-        List<Document> queryResult = collection.find(eq("orderPlaced", "2018-03-01")).into(new ArrayList<Document>());;
-			return queryResult;
-			
-		}
-		catch (Exception e) {
-			//log the exception
-			
-			System.out.println("An exception occured");
-			System.out.println("Details:");
-			System.out.println(e.getStackTrace());
-			return null;
-							
-		}		
-	}
-	
-	public static List<Document> CountOrderSize(String connectionString)
+	public List<Document> CountOrderSize(String connectionString)
 	{
 
 		if(connectionString == null || connectionString.isEmpty() )
@@ -240,11 +173,13 @@ public class InsertData {
 			
 			System.out.println("An exception occured");
 			System.out.println("Details:");
-			System.out.println(e.getStackTrace());
+			e.printStackTrace();
 			return null;
 							
 		}		
 	}
+
+	
 
 }
 
--- InsertData/testsrc/InsertDataTest.java
+++ InsertData/testsrc/InsertDataTest.java
@@ -7,7 +7,10 @@ import static org.junit.Assert.*;
 import org.bson.Document;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
@@ -19,7 +22,15 @@ import org.junit.Test;
 public class InsertDataTest {
 
 	String CONNECTION = "mongodb+srv://m001-student:student123#@sandbox-trhqa.mongodb.net/test";
+	String json = " 'orderPlaced': '2018-03-01', 'total': 13.00, 'subtotal': 191.00,"
+			+ "'shipping':11.00,'tax':17.00,"
+			+ "'status': ['shipped', '2018-03-04'],"
+			+ "'shippingAddress': {'number': 345, 'street': 'Broadway', 'city': 'Sunnyside',"
+			+"'state':'Yonkers','country':'USA','postalCode':10010},"
+			+"'lineitems':[{'sku':'MDBTS115','name':'color pencil','quantity':31,'unit_price':1.35},"
+			+"{'sku':'MDBTS438','name':'Highlighter','quantity':27,'unit_price':2.4}]";
 	
+	//Check the connection
 	@Test
 	public void TestConnection() {		
 		MongoClientURI clientUri = new MongoClientURI(CONNECTION);
@@ -28,44 +39,135 @@ public class InsertDataTest {
 			assertNotNull(client);
 		}    
 	}
+	
+	//Test for adding one data set
 	@Test
-	public void AddOneOrderTest() {
+	public void AddOneOrderWithDataTest() {
 		
+        Document newOrder = new Document("orderPlaced", "2018-03-01")
+        .append("total", 1633.2)
+        .append("subtotal",421.42)
+		.append("shipping", 91)
+		.append("tax", 11.41);
+
+     	List status = Arrays.asList("shipped","2018-03-10");
+		newOrder.put("status", status);
+
+		Document shipping = new Document ("number",113)
+		.append("street", "Sunnyside")
+		.append("city","Yonker")
+		.append("state","New York")
+		.append("country","USA")
+		.append("postalCode",57850);
+		newOrder.put("shippingAddress", shipping);	
+		
+		Document itemDetail = new Document("sku","MDBTS285")
+		.append("name","color pencil")
+		.append("quantity",32)
+		.append("unit_price",9.3808);
+		List items = Arrays.asList(itemDetail);
+		newOrder.put("lineitems", items);
+			 
 		InsertData insertData = new InsertData();		
-		List<Document> AddDocuments = InsertData.AddOneOrder(CONNECTION);
-		List<Document> filteredDocuments = InsertData.CountOrderSize(CONNECTION);
+		List<Document> filteredDocumentsBefore = insertData.CountOrderSize(CONNECTION);
+		InsertData.AddOneOrderWithData(CONNECTION, newOrder);
+		List<Document> filteredDocumentsAfter = insertData.CountOrderSize(CONNECTION);
 			
-		assertEquals(102, filteredDocuments.size());	
+		assertEquals(filteredDocumentsBefore.size()+1, filteredDocumentsAfter.size());	
 	}
 
 	
 	@Test(expected=IllegalArgumentException.class)
-	public void AddOneOrderTest_InvalidConnectionString_ThrowsException() 
+	public void AddOneOrderWithDataTest_InvalidConnectionString_ThrowsException() 
 	{
-		InsertData insertData = new InsertData();		
-		List<Document> AddDocuments = InsertData.AddOneOrder("");
-	    
+		Document newOrder = new Document("orderPlaced", "2018-03-01")
+        .append("total", 1633.2)
+        .append("subtotal",421.42)
+		.append("shipping", 91)
+		.append("tax", 11.41);
+
+     	List status = Arrays.asList("shipped","2018-03-10");
+		newOrder.put("status", status);
+
+		Document shipping = new Document ("number",113)
+		.append("street", "Sunnyside")
+		.append("city","Yonker")
+		.append("state","New York")
+		.append("country","USA")
+		.append("postalCode",57850);
+		newOrder.put("shippingAddress", shipping);	
+		
+		Document itemDetail = new Document("sku","MDBTS285")
+		.append("name","color pencil")
+		.append("quantity",32)
+		.append("unit_price",9.3808);
+		List items = Arrays.asList(itemDetail);
+		newOrder.put("lineitems", items);
+	
+		InsertData insertData = new InsertData();	
+		List<Document> AddDocuments = InsertData.AddOneOrderWithData("", newOrder );
 	}
 	
 	
 	@Test
-	public void AddOneOrderJsonTest() {
-		
-		InsertData insertData = new InsertData();		
-		List<Document> AddDocuments = InsertData.AddOneOrderJson(CONNECTION);
-		List<Document> filteredDocuments = InsertData.CountOrderSize(CONNECTION);
-		
+	public void AddOneOrderWithExpDataTest_withoutID() {
 	
-		assertEquals(101, filteredDocuments.size());	
+		String orderData ="{"+json+"}";		
+		InsertData insertData = new InsertData();		
+		List<Document> filteredDocumentsBefore = insertData.CountOrderSize(CONNECTION);
+		List<Document> AddDocuments = InsertData.AddOneOrderWithExpData(CONNECTION, orderData);
+		List<Document> filteredDocumentsAfter = insertData.CountOrderSize(CONNECTION);
+		// check if the data is added and the overall size has incremented by 1
+		assertEquals(filteredDocumentsBefore.size()+1, filteredDocumentsAfter.size());	
+		
 	}
-
+	
+	@Test
+	public void AddOneOrderWithExpDataTest_withID() {
+	
+		String orderData = "{ '_id': '7a999e1a1a11cc111fb1f99c'," +json+"}";
+					
+		InsertData insertData = new InsertData();		
+		List<Document> filteredDocumentsBefore = insertData.CountOrderSize(CONNECTION);
+		List<Document> AddDocuments = InsertData.AddOneOrderWithExpData(CONNECTION, orderData);
+		List<Document> filteredDocumentsAfter = insertData.CountOrderSize(CONNECTION);
+		// check if the data is added and the overall size has incremented by 1
+		assertEquals(filteredDocumentsBefore.size()+1, filteredDocumentsAfter.size());	
+		// check if the above data can be found in the collection. Meaning it is inserted
+		Document result = AddDocuments.get(filteredDocumentsAfter.size()-1);
+	    String id = result.getString("_id");
+	    assertEquals("7a999e1a1a11cc111fb1f99c",id);	
+	}
 	
 	@Test(expected=IllegalArgumentException.class)
-	public void AddOneOrderJsonTest_InvalidConnectionString_ThrowsException() 
+	public void AddOneOrderWithExpDataTest_InvalidConnectionString_ThrowsException() 
+	{
+		String orderData ="{"+json+"}";		
+		InsertData insertData = new InsertData();		
+		List<Document> AddDocuments = InsertData.AddOneOrderWithExpData("", orderData);
+	}
+	/*
+	@Test
+	public void AddOneOrderJsonFileTest() {
+		
+		InsertData insertData = new InsertData();		
+		List<Document> filteredDocumentsBefore = insertData.CountOrderSize(CONNECTION);
+		List<Document> AddDocuments = insertData.AddOneOrderJsonFile(CONNECTION,"simpledata.json");
+		List<Document> filteredDocumentsAfter = insertData.CountOrderSize(CONNECTION);
+		// check if the data is added and the overall size has incremented by 1
+	//	assertEquals(filteredDocumentsBefore.size()+1, filteredDocumentsAfter.size());	
+		Document result = AddDocuments.get(filteredDocumentsAfter.size()-1);
+	    String id = result.getString("_id");
+	    assertEquals("8a999e1a1a11cc111fb1f99c",id);	
+	}
+	
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void AddOneOrderJsonFileTest_ExistingID_ThrowsException() 
 	{
 		InsertData insertData = new InsertData();		
-		List<Document> AddDocuments = InsertData.AddOneOrder("");
+		List<Document> AddDocuments = InsertData.AddOneOrderJsonFile(CONNECTION,"simpledata.json");
 	    
-	}
-		
+	}*/
+
 }	
