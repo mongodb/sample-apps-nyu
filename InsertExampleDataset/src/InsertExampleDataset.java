1ffd056c8dd029ac75c350114beb9b86a1993137
Raghavendra Harish <rh2551@nyu.edu>
2018-04-29 15:20:23 -0400

    Added unit tests and re organised code

--- InsertExampleDataset/src/InsertExampleDataset.java
+++ InsertExampleDataset/src/InsertExampleDataset.java
@@ -1,5 +1,8 @@
+import static com.mongodb.client.model.Filters.eq;
+
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,8 +15,7 @@ import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-public class InsertExampleDataset {
-	
+public class InsertExampleDataset {	
 	public static void main(String[] args) throws IOException{
 		if(args.length != 2){
 			throw new IllegalArgumentException("Usage: InsertExampleDataset <Connection String> <json file path>");
@@ -22,26 +24,57 @@ public class InsertExampleDataset {
 		String connectionString = args[0];
 		String filePath = args[1];
 		
+		if(InsertDataset(connectionString,filePath)){
+			System.out.println("Done!");	
+		}
+		else{
+			System.out.println("Failed to insert dataset. Please check for error details");
+		}
+	}
+	
+	/**
+	 * Inserts documents to MongoDB.
+	 * @param connectionString: To MongoDB instance/MongoDB Cluster.
+	 * @param filePath: Path to Json file with records.
+	 * @return List<Document>: ArrayList of matching documents.
+	 * @throws FileNotFoundException 
+	 */
+	public static Boolean InsertDataset(String connectionString, String filePath) throws FileNotFoundException {
+		if(connectionString.isEmpty() || connectionString == null || filePath.isEmpty() || filePath == null){
+			throw new IllegalArgumentException();
+		}
+				
 		System.out.println("Reading file: "+ filePath);
 		BufferedReader reader = null;
 		List<Document> orders = new ArrayList<Document>();
-
-		try {
-		    File file = new File(filePath);
-		    reader = new BufferedReader(new FileReader(file));
+		
+		File file = new File(filePath);
+		if(!file.exists() || !file.isFile()){
+			throw new FileNotFoundException();
+		}
+			
+		try{
+			reader = new BufferedReader(new FileReader(file));
 
 		    String jsonString;
 		    while ((jsonString = reader.readLine()) != null) {
 		    	Document doc = Document.parse(jsonString);
 		    	orders.add(doc);
 		    }
-		} catch (IOException e) {
-		    e.printStackTrace();
+		}
+		catch(IOException e){
+			System.out.println("Exception occured");
+			System.out.println("Details:");
+			e.printStackTrace();
+			return false;
 		} finally {
 		    try {
 		        reader.close();
 		    } catch (IOException e) {
-		        e.printStackTrace();
+		    	System.out.println("Exception occured");
+				System.out.println("Details:");
+				e.printStackTrace();
+				return false;
 		    }
 		}
 		
@@ -54,12 +87,13 @@ public class InsertExampleDataset {
 			collection.deleteMany(new Document());
 			System.out.println("Adding new records");
 			collection.insertMany(orders);
-			System.out.println("Done!");
+			return true;			
 		}
 		catch (Exception e) {
 			System.out.println("Exception occured");
 			System.out.println("Details:");
-			System.out.println(e.getStackTrace());
-		}	
+			e.printStackTrace();
+			return false;
+		}				
 	}
 }
