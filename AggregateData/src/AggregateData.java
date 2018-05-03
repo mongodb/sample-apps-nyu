197eca720709ea46cb0208bae74c0c435bcc51d2
Meghana <md3822@nyu.edu>
2018-05-03 08:23:59 -0400

    Added Unit Test Cases

--- AggregateData/src/AggregateData.java
+++ AggregateData/src/AggregateData.java
@@ -17,118 +17,112 @@ import com.mongodb.client.model.Accumulators;
 import com.mongodb.client.model.Filters;
 
 public class AggregateData {
-	
-	 public static void main(String[] args)
-	{
-	System.out.println("hello");
-		//List<Document> res;
-		String CONNECTION = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox-rjrpn.mongodb.net/test";
-		String state = "Texas";
 		
-		AggregateData aggregateData = new AggregateData();
-		aggregateData.AggregateByState(CONNECTION,state);
-		aggregateData.AggregateByStateAverageShipping(CONNECTION, state);
-		aggregateData.AggregateByCityCheckStatus(CONNECTION);
-	
-	}
-	
-	 Block<Document> printBlock = new Block<Document>() {
-	        @Override
-	        public void apply(final Document document) {
-	            System.out.println(document.toJson());
-	        }
-	    };
-	
-	public void AggregateByState(String connectionString, String state){
-		if(connectionString == null || connectionString.isEmpty() || state == null || state.isEmpty())
-		{
-			throw new IllegalArgumentException();
-		}
-		else
-		{
-			MongoClientURI clientUri = new MongoClientURI(connectionString);
-			try(MongoClient client = new MongoClient(clientUri))
+		public List<Document> AggregateByState(String connectionString, String state){
+			if(connectionString == null || connectionString.isEmpty() || state == null || state.isEmpty())
 			{
-				MongoDatabase database = client.getDatabase("stores");			
-				MongoCollection<Document> collection = database.getCollection("orders");		
+				throw new IllegalArgumentException();
+			}
+			else
+			{
+				MongoClientURI clientUri = new MongoClientURI(connectionString);
+				try(MongoClient client = new MongoClient(clientUri))
+				{
+					MongoDatabase database = client.getDatabase("stores");			
+					MongoCollection<Document> collection = database.getCollection("orders");		
+					
+					List<Document> queryResult = collection.aggregate(
+						      		Arrays.asList(
+						      				Aggregates.match(Filters.eq("shippingAddress.state", state)),
+						      				Aggregates.group("$shippingAddress.city", Accumulators.sum("count", 1))
+						      				)
+							).into(new ArrayList<Document>());
+					return queryResult;
+				}
 				
-				collection.aggregate(
-					      Arrays.asList(
-					              Aggregates.match(Filters.eq("shippingAddress.state", state)),
-					              Aggregates.group("$shippingAddress.city", Accumulators.sum("count", 1))
-					      )
-					).forEach(printBlock);
-			}
-			
-			catch (Exception e) 
-			{
-				System.out.println("Exception occured");
-				System.out.println("Details:");
-				e.printStackTrace();
-			}	
-		}			
-	}
-	
-	public void AggregateByStateAverageShipping(String connectionString, String state){
-		if(connectionString == null || connectionString.isEmpty() || state == null || state.isEmpty())
-		{
-			throw new IllegalArgumentException();
+				catch (Exception e) 
+				{
+					System.out.println("Exception occured");
+					System.out.println("Details:");
+					e.printStackTrace();
+					return null;
+				}	
+			}			
 		}
-		else
-		{
-			MongoClientURI clientUri = new MongoClientURI(connectionString);
-			try(MongoClient client = new MongoClient(clientUri))
+	
+		public List<Document> AggregateByStateAverageShipping(String connectionString, String state){
+			if(connectionString == null || connectionString.isEmpty() || state == null || state.isEmpty())
 			{
-				MongoDatabase database = client.getDatabase("stores");			
-				MongoCollection<Document> collection = database.getCollection("orders");		
+				throw new IllegalArgumentException();
+			}
+			else
+			{
+				MongoClientURI clientUri = new MongoClientURI(connectionString);
+				try(MongoClient client = new MongoClient(clientUri))
+				{
+					MongoDatabase database = client.getDatabase("stores");			
+					MongoCollection<Document> collection = database.getCollection("orders");		
+					
+					List<Document> queryResult = collection.aggregate(
+						      Arrays.asList(
+						              Aggregates.match(Filters.eq("shippingAddress.state", state)),
+						              Aggregates.group("$shippingAddress.state", Accumulators.avg("Average Shipping", "$shipping"))
+						      )
+						).into(new ArrayList<Document>());
+					return queryResult;
+					
+				}
 				
-				collection.aggregate(
-					      Arrays.asList(
-					              Aggregates.match(Filters.eq("shippingAddress.state", state)),
-					              Aggregates.group("$shippingAddress.state", Accumulators.avg("Average Shipping", "$shipping"))
-					      )
-					).forEach(printBlock);
-			}
-			
-			catch (Exception e) 
-			{
-				System.out.println("Exception occured");
-				System.out.println("Details:");
-				e.printStackTrace();
-			}	
-		}			
-	}
-	
-	
-	public void AggregateByCityCheckStatus(String connectionString){
-		if(connectionString == null || connectionString.isEmpty())
-		{
-			throw new IllegalArgumentException();
+				catch (Exception e) 
+				{
+					System.out.println("Exception occured");
+					System.out.println("Details:");
+					e.printStackTrace();
+					return null;
+				}	
+			}			
 		}
-		else
-		{
-			MongoClientURI clientUri = new MongoClientURI(connectionString);
-			try(MongoClient client = new MongoClient(clientUri))
-			{
-				MongoDatabase database = client.getDatabase("stores");			
-				MongoCollection<Document> collection = database.getCollection("orders");
 	
-				collection.aggregate(
-						Arrays.asList(
-								Aggregates.project(
-										Projections.fields(
-												Projections.excludeId(),
-												Projections.include("shippingAddress.city"),
-												Projections.computed(
-														"firstCategory",
-														new Document("$arrayElemAt", Arrays.asList("$status", 0))
-		                    )
-		              )
-		          )
-				)
-				).forEach(printBlock);
+	
+		public List<Document> AggregateByCityCheckStatus(String connectionString){
+			if(connectionString == null || connectionString.isEmpty())
+			{
+				throw new IllegalArgumentException();
+			}
+			else
+			{
+				MongoClientURI clientUri = new MongoClientURI(connectionString);
+				try(MongoClient client = new MongoClient(clientUri))
+				{
+					MongoDatabase database = client.getDatabase("stores");			
+					MongoCollection<Document> collection = database.getCollection("orders");
+		
+					List<Document> queryResult = collection.aggregate(
+							Arrays.asList(
+									Aggregates.project(
+											Projections.fields(
+													Projections.excludeId(),
+													Projections.include("shippingAddress.city"),
+													Projections.computed(
+															"firstCategory",
+															new Document("$arrayElemAt", Arrays.asList("$status", 0))
+			                    )
+			              )
+			          )
+					)
+					).into(new ArrayList<Document>());
+					return queryResult;
+				}
+					
+					catch (Exception e) 
+					{
+						System.out.println("Exception occured");
+						System.out.println("Details:");
+						e.printStackTrace();
+						return null;
+					}
+
 			}
 		}
-	}
 }
 				
